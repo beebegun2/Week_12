58a80385e85b307af551a21363473504e4349ccc
Shannon Beebe <beebegun2.0@gmail.com>
2022-10-21 19:51:57 -0500

    week 12 project

--- week12project.js
+++ week12project.js
@@ -34,9 +34,9 @@ class TeaService {
     }
 
     static updateTea(tea) {
-        //console.log("is this tea working", Tea);
+        console.log("is this tea working", Tea);
         return $.ajax({
-            url: this.url + `/${tea._id}`,
+            url: this.url + `/${tea.id}`,
             dataType: 'json',
             data: JSON.stringify(tea),
             contentType: 'application/json',
@@ -45,7 +45,7 @@ class TeaService {
     }
 
     static deleteTea(id) {
-        //console.log('testing the delete', Tea);
+        console.log('testing the delete', Tea);
         return $.ajax({
             url: this.url + `/${id}`,
             type: 'DELETE'
@@ -70,6 +70,13 @@ class DOMManager {
         this.render(teas)
     });
 }
+
+    static updateTea(name){
+    TeaService.updateTea(new Tea(name))
+    .then(() => {
+        console.log('this updated the tea name', tea);
+    });
+    }
 
     static deleteTea(id) {
     TeaService.deleteTea(id)
@@ -120,37 +127,42 @@ class DOMManager {
         this.teas = teas;
         $('#app').empty();
         for (let tea of teas) { //this loops thru the array of teas and each element is called tea
-            console.log('tea', teas);
+            console.log('tea', tea);
+
             $('#app').prepend(
-            `<div id="${tea._id}" class="card">
+            `<div id="${tea.id}" class="card">
                 <div class="card-header">
                     <h2>${tea.name}</h2>
-                    <button class="btn btn-danger" onclick="DOMManager.deleteTea('${tea._id}')">Delete</button>
+                    <div class="col-sm">
+                                <input type ="text" id="${tea.id}-tea-name" class ="form-control" placeholder="Updated Tea Name">
+                            </div>
+                    <button class="btn btn-secondary" onclick="DOMManager.updateTea('${tea.id}')">Update</button>
+                    <button class="btn btn-danger" onclick="DOMManager.deleteTea('${tea.id}')">Delete</button> 
                 </div>
                 <div class="card-body">
                     <div class="card">
                         <div class="row">
+                                <div class="col-sm">
+                                    <input type ="text" id="${tea.id}-flavor-name" class ="form-control" placeholder="Flavor Name">
+                                </div>
+                                
                             <div class="col-sm">
-                                <input type ="text" id="${tea._id}-flavor-name" class ="form-control" placeholder="Flavor Name">
-                        
-                            </div>
-                            <div class="col-sm">
-                                <input type ="text" id="${tea._id}-flavor-topping" class ="form-control" placeholder="Flavor Topping">
+                                <input type ="text" id="${tea.id}-flavor-topping" class ="form-control" placeholder="Flavor Topping">
                             </div>
                         </div>
-                        <button id="${tea._id}-new-flavor" onclick="DOMManager.addFlavor('${tea._id}')" class="btn btn-primary form-control">Add</button>
+                        <button id="${tea.id}-new-flavor" onclick="DOMManager.addFlavor('${tea.id}')" class="btn btn-primary form-control">Add</button>
                     </div>
                 </div>
             </div><br>`
          );
          for (let flavor of tea.flavor) {
-            //console.log("is this defined", flavor);
+            console.log("is this defined", flavor);
            $(`#${tea._id}`).find('.card-body').append(
                 `
                 <p>
                 <span id="name-${flavor._id}"><strong>Name: </strong> ${flavor.name}</span>
                 <span id="name-${flavor._id}"><strong>Topping: </strong> ${flavor.topping}</span>
-                <button class="btn btn-danger" onclick="DOMManager.deleteFlavor('${tea._id}','${flavor._id}')">Delete Flavor</button>
+                <button class="btn btn-danger" onclick="DOMManager.deleteFlavor('${tea.id}','${flavor._id}')">Delete Flavor</button>
                 </p>
                 `
             )
@@ -159,7 +171,7 @@ class DOMManager {
  }
 }
 
-$('#create-new-tea').click(() => {
+$('#create-new-tea').on(() => {
     DOMManager.createTea($('#new-tea-name').val());
     $('#new-tea-name').val('');
 });
